0c36662d64de60ef4ec9cce0a13cd43f4fb2091a
FrancisRalph <francis341593@gmail.com>
2023-06-04 00:55:31 +0800

    implement object detection

--- app/api.py
+++ app/api.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
 from fastapi import FastAPI, UploadFile
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 import uvicorn
 from ultralytics import YOLO
-import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 cwd = Path.cwd()
 app_path = cwd / 'app'
@@ -12,32 +13,59 @@ models_path = app_path / 'models'
 website_path = app_path / 'website'
 
 food_classification_model_path = models_path / 'food_classification.pt'
+food_classification_model = YOLO(str(food_classification_model_path.absolute()))
+
+object_detection_model = YOLO('yolov8n.pt')
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return "Nothing to see here"
-
-
 @app.post("/classify")
 async def classify(image: UploadFile):
-    model = YOLO(str(food_classification_model_path.absolute()))
+    with Image.open(image.file) as im:
+        print(im.mode)
+        im = im.convert('RGB') # handle .png files
+        print(im.mode)
 
-    with Image.open(image.file) as image_pil:
-        results = model(image_pil)
-    
-    result = results[0]
+        # run object detection inference
+        object_detection_result = object_detection_model(im, conf=0.1)[0]
 
-    output = {}
+        # classify objects in each box detected and draw on image for result
+        draw = ImageDraw.Draw(im)
+        boxes = object_detection_result.boxes
+        for box in boxes:
+            # classify object in box
+            class_name = object_detection_result.names[int(box.cls)]
+            bounding_box = box.xyxy[0].numpy()
+            cropped = im.crop(bounding_box)
+            
+            classification_result = food_classification_model(cropped)[0]
+            top3_classes = []
+            for i, class_index in enumerate(classification_result.probs.top5[:3]):
+                class_name = classification_result.names[class_index]
+                class_confidence = classification_result.probs.top5conf[i]
+                top3_classes.append({'class_name': class_name, 'class_confidence': class_confidence})
 
-    for i, contender in enumerate(result.probs.top5):
-        class_name = result.names[contender]
-        class_confidence = result.probs.top5conf[i]
-        output[class_name] = round(float(class_confidence), 4)
+            # draw on image for result
+            draw.rectangle(bounding_box)
 
-    return output
+            info_lines = [f'{info["class_name"]} ({info["class_confidence"] * 100:.2f}%)' for info in top3_classes]
+            info_lines.append(f"box.conf: {float(box.conf) * 100:.2f}%")
 
+            font = ImageFont.truetype('arial.ttf', size=10)
+            draw.multiline_text(
+                bounding_box[:2],
+                "\n".join(info_lines),
+                font=font,
+                fill=(255, 255, 255),
+                stroke_width=1,
+                stroke_fill=(0, 0, 0)
+            )
+
+        im.save(website_path / 'result.jpg')
+
+    return RedirectResponse('/', status_code=303)
+
+app.mount("/", StaticFiles(directory=website_path, html=True), name="static")
 
 if __name__ == '__main__':
     uvicorn.run("api:app", reload=True)
---
+++ scripts/object_detection.py
@@ -0,0 +1,71 @@
+from ultralytics import YOLO
+import cv2
+from PIL import Image, ImageDraw
+import pandas as pd
+
+from common import cwd, datasets_path
+
+def main():
+    model = YOLO('yolov8n.pt')
+    # image_to_predict_path = datasets_path / 'prannays_edibles_extended' / 'train' / 'dessert' / '2_3.jpg'
+    image_to_predict_path = cwd / 'images_to_predict' / 'prannays_edibles' / 'wholesomeyum-Perfect-Grilled-Sirloin-Steak-500x500.jpg'
+    img = Image.open(image_to_predict_path)
+    results = model(img, classes=[i for i in range(39, 56)], conf=0.1)
+    for result in results:
+        boxes = result.boxes
+        for box in boxes:
+            class_name = result.names[int(box.cls)]
+            bounding_box = box.xyxy[0].numpy()
+            cropped = img.crop(bounding_box)
+            # cropped.show()
+
+        result_plotted = result.plot()
+        cv2.imshow("result", result_plotted)
+        cv2.waitKey()
+
+
+def test():
+    dataset_path = datasets_path / 'UECFOOD100'
+    image_no = 14927
+    image_class_idx = 94
+    test_image_path = dataset_path / f'{image_class_idx}' / f'{image_no}.jpg'
+    
+    bb_info_path = dataset_path / f'{image_class_idx}' / 'bb_info.txt'
+    bb_info_df = pd.read_csv(bb_info_path, delim_whitespace=True)
+    
+    image_bb = bb_info_df.loc[bb_info_df['img'] == image_no].iloc[0, 1:].to_numpy()
+    
+    with Image.open(test_image_path) as im:
+        draw = ImageDraw.Draw(im)
+        draw.rectangle(tuple(image_bb))
+        im.show()
+
+
+"""
+Food-related classes:
+
+"39":"bottle",
+"40":"wine glass",
+"41":"cup",
+"42":"fork",
+"43":"knife",
+"44":"spoon",
+"45":"bowl",
+"46":"banana",
+"47":"apple",
+"48":"sandwich",
+"49":"orange",
+"50":"broccoli",
+"51":"carrot",
+"52":"hot dog",
+"53":"pizza",
+"54":"donut",
+"55":"cake",
+
+"60":"dining table",
+"""
+
+
+if __name__ == '__main__':
+    main()
+    # test()
